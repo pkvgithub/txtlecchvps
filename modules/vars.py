b127f80c166ee23dfca0287dcdebdffab06255ec
pkvgithub <145007232+pkvgithub@users.noreply.github.com>
2024-04-12 12:29:06 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "25129557"
 api_hash = "83c9546cfdee154fd2d16759c4d0582a"
-bot_token = "7128870610:AAGb94tm3PmrFTXrA3GWHSSSRt77Mezjrqs"
+bot_token = "6684414701:AAEFqD4DVz2fd4dBVQgHsF3fJZM4lqBhies"
