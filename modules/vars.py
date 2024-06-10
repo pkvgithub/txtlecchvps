1a26d1649a6b8eb847ab796799d03a28a2fc268a
pkvgithub <145007232+pkvgithub@users.noreply.github.com>
2024-06-10 15:23:32 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "25129557"
 api_hash = "83c9546cfdee154fd2d16759c4d0582a"
-bot_token = "6684414701:AAEFqD4DVz2fd4dBVQgHsF3fJZM4lqBhies" #uploder v1
+bot_token = "7225310951:AAHO3SaF9IqrklJU6NAmC4tXdT9mxRpSJe4" #uploder v1
