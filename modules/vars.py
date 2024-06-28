12f46ed4c16da0158d37497de960c492bb5fa82e
pkvgithub <145007232+pkvgithub@users.noreply.github.com>
2024-06-28 14:43:00 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = ""
-api_hash = ""
-bot_token = "" #uploder v1
+API_ID    = os.environ.get("API_ID", "")
+API_HASH  = os.environ.get("API_HASH", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
