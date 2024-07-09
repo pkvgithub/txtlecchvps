f599a6c34972db6ef4c965210cecef5e80c3102b
pkvgithub <145007232+pkvgithub@users.noreply.github.com>
2024-07-09 17:53:57 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,7 +1,7 @@
 import os
 
-API_ID    = os.environ.get("API_ID", "")
-API_HASH  = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+API_ID    = os.environ.get("API_ID", "25129557")
+API_HASH  = os.environ.get("API_HASH", "83c9546cfdee154fd2d16759c4d0582a")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7014436725:AAH1uW3D79o0vMmphVgGE_1LQwIFxUj7pZY") 
 WEBHOOK = True  # Don't change this
 PORT = int(os.environ.get("PORT", 8080))  # Default to 8000 if not set
